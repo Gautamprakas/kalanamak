59b5b70b3e85c02f9e33af15d20d8f852802411c
Prakash <gautamprakash697@gmail.com>
2024-09-10 17:33:31 +0530

    Added navbar and bootstrap css

--- src/App.js
+++ src/App.js
@@ -3,22 +3,55 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Hi,All this is prakash gautam</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Kalanamk Rice</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About Us</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Contact</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><hr className="dropdown-divider"/></li>
+          </ul>
+        </li>
+        
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
+<div className="card text-center">
+  <div className="card-header">
+    Featured
+  </div>
+  <div className="card-body">
+    <h5 className="card-title">Chlo fir leto</h5>
+    <p className="card-text">Reel nhi chalegi</p>
+    <a href="#" class="btn btn-primary"></a>
+  </div>
+  <div className="card-footer text-muted">
+    Hello Ranjeet
+  </div>
+</div>
+    </>
   );
 }
 
