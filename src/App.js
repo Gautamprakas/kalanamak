67b7d59c841978060d042e850487c70777c765df
Prakash <gautamprakash697@gmail.com>
2024-09-11 14:13:22 +0530

    Add components to our app,add props eg I have added a Navbar.js file inside compoents folder that is our component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Kalanamk Rice</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About Us</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Contact</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><hr className="dropdown-divider"/></li>
-          </ul>
-        </li>
-        
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-<div className="card text-center">
-  <div className="card-header">
-    Featured
-  </div>
-  <div className="card-body">
-    <h5 className="card-title">Chlo fir leto</h5>
-    <p className="card-text">Reel nhi chalegi</p>
-    <a href="#" class="btn btn-primary"></a>
-  </div>
-  <div className="card-footer text-muted">
-    Hello Ranjeet
-  </div>
-</div>
+    
+    <Navbar title="Kalanamak Rice" about="About us"/>
     </>
   );
 }
