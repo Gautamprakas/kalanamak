afb588c094726331ab4beeef6e45a07c5d55e061
Prakash <gautamprakash697@gmail.com>
2024-09-13 16:29:23 +0530

    Add about.js in component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 // import logo from './logo.svg'
 import './App.css'
 import Navbar from './components/Navbar.js'
-import TextForm from './components/TextForm.js'
-import TypingSpeedCalculator from './TypingSpeedCalculator.js'
+// import TextForm from './components/TextForm.js'
+import About from './components/About.js'
+// import TypingSpeedCalculator from './TypingSpeedCalculator.js'
 function App() {
   return (
     <>
     
     <Navbar title="Typing Master" about="About us"/>
     <div className="container my-3">
-      <TextForm heading="Enter Text To Analyze"/>
+      
+      <About/>
     </div>
     </>
   );
 }
 
 export default App;
+// <TextForm heading="Enter Text To Analyze"/>
