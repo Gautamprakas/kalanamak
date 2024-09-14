40509688d60ae3676b2769007b7ad009f52be321
Prakash <gautamprakash697@gmail.com>
2024-09-14 15:11:01 +0530

    Added fully dark-mode with the implementation of swtich

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 // import logo from './logo.svg'
 import './App.css'
+import React,{useState} from 'react'
 import Navbar from './components/Navbar.js'
-// import TextForm from './components/TextForm.js'
+import TextForm from './components/TextForm.js'
 import About from './components/About.js'
 // import TypingSpeedCalculator from './TypingSpeedCalculator.js'
 function App() {
+  const [mode,setMode]=useState("light");
+  const toggleSwitch=()=>{
+    console.log(mode);
+    if(mode=='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#4c3f6d';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
+  
   return (
     <>
     
-    <Navbar title="Typing Master" about="About us"/>
+    <Navbar title="Typing Master" about="About us" mode={mode} toggleSwitch={toggleSwitch} />
     <div className="container my-3">
       
-      <About/>
+      <TextForm mode={mode} heading='Enter text to Analyze'/>
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,17 +15,23 @@ export default function TextForm(props){
 	const handleResetClick=()=>{
 		setText('');
 	};
+	const handleCopy=()=>{
+		//console.log('I am copy');
+		var textElement=document.getElementById('textArea');
+		textElement.select();
+		navigator.clipboard.writeText(textElement.value);
+	}
 	const handleOnChange=(event)=>{
 		setText(event.target.value);
 		// console.log("On change");
 	};
 	return (
 		<>
-			<div className="container my-8">
+			<div className="container my-8" style={{color:props.mode=='dark'? 'white':'black'}}>
 				<h1>{props.heading}</h1>
 				<div className="mb-3">
 				  
-				  <textarea className="form-control" value={text} onChange={handleOnChange} id="textArea" rows="8"></textarea>
+				  <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode=='dark'? 'grey':'white'}} id="textArea" rows="8"></textarea>
 				</div>
 				<button className="btn btn-primary mx-2" onClick={handleUpClick}>
 				Convert To UpperCase
@@ -36,14 +42,17 @@ export default function TextForm(props){
 				<button className="btn btn-outline-danger btn-sm mx-2" onClick={handleResetClick}>
 					Reset 
 				</button>
+				<button className="btn btn-outline-primary btn-sm mx-2" onClick={handleCopy}>
+					Copy 
+				</button>
 			</div>
-			<div className="container my-3">
+			<div className="container my-3" style={{color:props.mode=='dark'? 'white':'black'}}>
 				<h2>Your Text Summary</h2>
 				<p><b>{text.split(" ").length -1} </b> words found, <b> {text.length} </b> characters found</p>
 			</div>
-			<div className="container my-3">
+			<div className="container my-3" style={{color:props.mode=='dark'? 'white':'black'}}>
 				<h2>Preview</h2>
-				<p>{text}</p>
+				<p>{text.length>0?text:'Enter something to Preview..'}</p>
 			</div>
 			
 		</>
